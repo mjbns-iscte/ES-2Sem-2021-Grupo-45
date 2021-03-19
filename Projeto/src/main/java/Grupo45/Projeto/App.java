02fe53d95d81a5419bb750ca9f2a66b9df45a48f
mjbns-iscte <mjbns@iscte-iul.pt>
2021-03-19 17:12:26 +0000

    maven test2

--- Projeto/src/main/java/Grupo45/Projeto/App.java
+++ Projeto/src/main/java/Grupo45/Projeto/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldddd!" );
+        System.out.println( "Hello Worlddddd!" );
     }
 }
