3a0a991c98fbf4f345cdad8ffc6d2248a8d8cf88
tmsgo-iscte <tmsgo@iscte-iul.pt>
2021-03-29 22:48:56 +0100

    Criação da classe LOC_method e alteração da Metricas_Metodos.
    -Criação da classe LOC_method
    -Divisão do código antigo entre a LOC_method e a Metricas_Metodos
    -Finalização e aperfeiçoamente do código
    
    Simplificação do código anteriormente criado, através da divisão do
    código para outra classe. A implementação do LOC_method deverá ser feita
    pela classe LOC_method e o Metricas_Metodos será usado para testes e
    como centro de todas as ações dos métodos, mas será futuramente
    eliminado ser substituido por uma classe mais adequada a todos os
    métodos.

--- Projeto/src/main/java/Grupo45/Projeto/Metricas_Metodos.java
+++ Projeto/src/main/java/Grupo45/Projeto/Metricas_Metodos.java
@@ -9,49 +9,89 @@ import org.w3c.dom.traversal.NodeIterator;
 import com.github.javaparser.ParseException;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.InstanceOfExpr;
 import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.MethodReferenceExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 
 
 
 public class Metricas_Metodos {
-	private ArrayList<String> methodNamesLOC = new ArrayList<>();
+	private ArrayList<ArrayList<String>> al = new ArrayList<ArrayList<String>>();
+	private ArrayList<String> a = new ArrayList<String>();
 	
-	private ArrayList<String> method_LOC(InputStream is) throws Exception{
-		methodNamesLOC.clear();
+	public Metricas_Metodos() {
+		super();
+		al.add(new ArrayList<String>());
+		al.add(new ArrayList<String>());
 		
-        CompilationUnit cu = StaticJavaParser.parse(is);
-        new VoidVisitorAdapter<Object>() {
-            @Override
-            public void visit(MethodDeclaration method, Object arg) {
-                super.visit(method, arg);
-                methodNamesLOC.add(method.getNameAsString());
-                methodNamesLOC.add(String.valueOf(method.getEnd().get().line - method.getBegin().get().column));
-            }
-        }.visit(cu, null);
-    
-    return methodNamesLOC;
-}
-	public ArrayList<String> getMethodNamesLOC() {
-		return methodNamesLOC;
+	}
+
+//	private ArrayList<String> method_LOC(InputStream is) throws Exception{
+//		methodNamesLOC.clear();
+//		
+//        CompilationUnit cu = StaticJavaParser.parse(is);
+//       
+//        System.out.println(1);--
+//        new VoidVisitorAdapter<Object>() {
+//            public void visit(MethodDeclaration method, Object arg) {
+//                super.visit(method, arg);
+//                System.out.println(1);
+//                methodNamesLOC.add(method.getNameAsString());
+//                methodNamesLOC.add(String.valueOf(method.getEnd().get().line - method.getBegin().get().line +1));
+//            }
+//            public void visit(ConstructorDeclaration method, Object arg) {
+//                super.visit(method, arg);
+//                System.out.println(1);
+//                methodNamesLOC.add(method.getNameAsString());
+//                methodNamesLOC.add(String.valueOf(method.getEnd().get().line - method.getBegin().get().line +1));
+//            }
+//        
+//        }.visit(cu,null);
+//        
+//    return methodNamesLOC;
+//}
+	public void analyze(File file) throws FileNotFoundException {
+		 InputStream is = new FileInputStream(file);
+		 CompilationUnit cu = StaticJavaParser.parse(is);
+		 new LOC_method().visit(cu, al);
+		 
 	}
 	
+
+
+
+
+	public ArrayList<ArrayList<String>> getAl() {
+		return al;
+	}
+
+	//	
     public static void main(String[] args) throws FileNotFoundException, Exception {
+    	//File file = new File("src/main/java/Grupo45/Projeto/Metricas_Metodos.java");
+        File file = new File("C://jasml//src//com//jasml//compiler//ParsingException.java");
+    	
     	Metricas_Metodos mm = new Metricas_Metodos();
-        File file = new File("src/main/java/Grupo45/Projeto/Metricas_Metodos.java");
-        InputStream is = new FileInputStream(file);
-        mm.method_LOC(is);
-        for(int i = 0; i < mm.getMethodNamesLOC().size(); i++) {   
-            System.out.print(mm.getMethodNamesLOC().get(i));
-        } 
-       //new LOC_Method().visit(StaticJavaParser.parse(file), null);
-        
+    	mm.analyze(file);
+    	
+        for(int i=0;i!=mm.al.get(0).size();i++) {
+        	System.out.println(mm.al.get(0).get(i));
+        	System.out.println(mm.al.get(1).get(i));
+        }
     }
 
 	
