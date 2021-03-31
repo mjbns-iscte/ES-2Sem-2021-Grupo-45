13dd694a776f4952433d0f75ad1c16afbde34e65
glhss-iscte <glhss@iscte-iul.pt>
2021-03-31 15:16:50 +0100

    Alterações no Cyclo_method
    acrescentei as seguintes funções para contar o numero de :
    -ifs
    -whiles
    -switches
    
    Finalizei o main e alterei o método add da classe Counter

--- Projeto/src/main/java/Grupo45/Projeto/Counter.java
+++ Projeto/src/main/java/Grupo45/Projeto/Counter.java
@@ -6,8 +6,12 @@ public class Complex {
 	private HashMap<String, Integer> map = new HashMap<>();
 	
 	public void add(String method_name) {
+		if(map.containsKey(method_name)) {
 		map.put(method_name, map.get(method_name) + 1);
-		
+		}
+		else {
+			map.put(method_name, 1);
+		}
 	}
 	
 	
