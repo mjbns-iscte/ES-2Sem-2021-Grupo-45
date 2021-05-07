fdb2ad8a8cdf99e7749b07d0508b7205fad14d46
mjbns-iscte <mjbns@iscte-iul.pt>
2021-05-07 14:34:50 +0100

    Criação dos comentários para o JavaDoc:
    Criei os comentários para o JavaDoc das classes Excel e Cyclo_method;

--- Projeto/src/main/java/Grupo45/Projeto/Cyclo_method.java
+++ Projeto/src/main/java/Grupo45/Projeto/Cyclo_method.java
@@ -16,15 +16,39 @@ import com.github.javaparser.ast.stmt.SwitchStmt;
 import com.github.javaparser.ast.stmt.WhileStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
+/**
+ * @author G45
+ * @author Andre Amado, Guilherme Henriques, João Guerra, Miguel Nunes, Francisco Mendes, Tiago Geraldo
+ * @version 1.0 
+ * This class is responsible to count the cyclomatic complexity of given methods
+ */
 public class Cyclo_method {
+
+	/**
+	 * This attribute saves the names of the methods in an array
+	 */
 	private static ArrayList<String> names = new ArrayList<String>();
+
+	/**
+	 * This string represents the name of one method
+	 */
 	private String method_name;
 
+	/**
+	 * @param is the file to be analyzed
+	 * @return a map with Strings, which represents the name of the methods, and Integers that represent the count of the cyclomatic complexity for
+	 * each method;
+	 */
 	public HashMap<String, Integer> cyclo_method(InputStream is) {
 
 		CompilationUnit cu = StaticJavaParser.parse(is);
 		Counter c = new Counter();
 		new VoidVisitorAdapter<Counter>() {
+
+			/**
+			 * this method is responsible to visit and save the name of all methods in an array and a map, and add 1 to the cyclomatic complexity 
+			 * of the method;
+			 */
 			public void visit(MethodDeclaration method, Counter c) {
 				method_name = method.getSignature().asString();
 				names.add(method_name);
@@ -32,6 +56,10 @@ public class Cyclo_method {
 				c.add(method_name);
 			}
 
+			/**
+			 * this method is responsible to visit and save the name of all constructors in an array and a map, and add 1 to the cyclomatic complexity 
+			 * of the constructor;
+			 */
 			public void visit(ConstructorDeclaration method, Counter c) {
 				method_name = method.getSignature().asString();
 				names.add(method_name);
@@ -39,33 +67,58 @@ public class Cyclo_method {
 				c.add(method_name);
 			}
 
+			/**
+			 * this method is responsible to visit all the methods, count the number of ForEach statements and add that number to the cyclomatic 
+			 * complexity of that method;
+			 */
 			public void visit(ForEachStmt statement, Counter c) {
 				c.add(method_name);
 				super.visit(statement, c);
 			}
 
+			/**
+			 * this method is responsible to visit all the methods, count the number of For statements and add that number to the cyclomatic 
+			 * complexity of that method;
+			 */
 			public void visit(ForStmt statement, Counter c) {
 				c.add(method_name);
 				super.visit(statement, c);
 			}
 
+			/**
+			 * this method is responsible to visit all the methods, count the number of Do statements and add that number to the cyclomatic 
+			 * complexity of that method;
+			 */
 			public void visit(DoStmt statement, Counter c) {
 				c.add(method_name);
 				super.visit(statement, c);
 			}
 
+			/**
+			 * this method is responsible to visit all the methods, count the number of While statements and add that number to the cyclomatic 
+			 * complexity of that method;
+			 */
 			public void visit(WhileStmt statement, Counter c) {
 				c.add(method_name);
 				super.visit(statement, c);
 			}
 
+			/**
+			 * this method is responsible to visit all the methods, count the number of IF statements and add that number to the cyclomatic 
+			 * complexity of that method;
+			 */
 			public void visit(IfStmt statement, Counter c) {
 				c.add(method_name);
 				super.visit(statement, c);
 			}
 
+			/**
+			 * this method is responsible to visit all the methods, count the number of Switch statements and add that number to the cyclomatic 
+			 * complexity of that method;
+			 */
 			public void visit(SwitchStmt statement, Counter c) {
-				for (@SuppressWarnings("unused") SwitchEntry s : statement.getEntries()) {
+				for (@SuppressWarnings("unused")
+				SwitchEntry s : statement.getEntries()) {
 					c.add(method_name);
 				}
 				super.visit(statement, c);
@@ -74,8 +127,10 @@ public class Cyclo_method {
 		return c.getMap();
 	}
 
+	/**
+	 * @return the array which contains the names of all methods;
+	 */
 	public static ArrayList<String> getNames() {
 		return names;
 	}
-
 }
--- Projeto/src/main/java/Grupo45/Projeto/Excel.java
+++ Projeto/src/main/java/Grupo45/Projeto/Excel.java
@@ -11,13 +11,33 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+/**
+ * @author G45
+ * @author Andre Amado, Guilherme Henriques, João Guerra, Miguel Nunes, Francisco Mendes, Tiago Geraldo
+ * @version 1.0 
+ * This class was constructed to build an Excel file with a specific format  
+ */
 public class Excel {
 
+	/**
+	 * This vector saves the name of the columns in the excel file
+	 */
 	private static String[] columns = { "MethodID", "package", "class", "method", "NOM_class", "LOC_class", "WMC_class",
 			"LOC_method", "CYCLO_method" };
+	/**
+	 * This attribute represents the Excel Sheet of our Excel file 
+	 */
 	private org.apache.poi.ss.usermodel.Sheet s;
+	/**
+	 * This attribute saves the path where Excel will be saved
+	 */
 	private String g_path;
 
+	/**
+	 * @param path where Excel will be saved
+	 * This method is responsible to build the Excel file and save him on the path
+	 * @throws IOException
+	 */
 	public void setupExcel(String path) throws IOException {
 		Workbook w = new XSSFWorkbook();
 		g_path = path;
@@ -50,18 +70,31 @@ public class Excel {
 		w.close();
 	}
 
+	/**
+	 * @return the chosen path
+	 */
 	public String getG_path() {
 		return g_path;
 	}
 
+	/**
+	 * @return the Excel sheet of our file
+	 */
 	public org.apache.poi.ss.usermodel.Sheet getSheet() {
 		return s;
 	}
 
+	/**
+	 * @return the number of columns in the excel file
+	 */
 	public int getHeaderSize() {
 		return columns.length;
 	}
 
+	/**
+	 * @param s is the name of a specific metric
+	 * @return the number of the column where that metric is
+	 */
 	public int getMetricColumn(String s) {
 		int out = -1;
 		for (int i = 0; i != columns.length; i++) {
